0f8be2a53bfc8917bba3dd7e54c757f496200e7f
Joonhoo <joonhoo0123@gmail.com>
2025-02-26 01:03:00 +0900

    modify: popup 으로 측정시 터치 영역 침범 -> SubComponent 방식으로 변경

--- Giunne/common/src/commonMain/kotlin/com/project/giunne/common/presentation/common/charactor/GPCharacter.kt
+++ Giunne/common/src/commonMain/kotlin/com/project/giunne/common/presentation/common/charactor/GPCharacter.kt
@@ -10,20 +10,21 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.layout.SubcomposeLayout
 import androidx.compose.ui.layout.onSizeChanged
-import androidx.compose.ui.window.Popup
+import androidx.compose.ui.unit.IntSize
 import coil3.compose.AsyncImage
+import com.project.giunne.BuildKonfig
 import com.project.giunne.common.data.remote.response.Item
-import com.project.giunne.common.data.util.DefineUrl.IMAGE_BASE_URL
 import com.project.giunne.common.util.gdp
 
 @Composable
 fun GPCharacter(
     modifier: Modifier = Modifier,
+    currentLevel: Int,
     character: String,
     items: List<Item> = listOf()
 ) {
-
     Box {
         AsyncImage(
             modifier = modifier,
@@ -31,38 +32,53 @@ fun GPCharacter(
             contentDescription = null
         )
         items.forEach { item ->
-            var width by remember { mutableStateOf(0) }
-            var height by remember { mutableStateOf(0) }
             // 이미지 크기만 측정 -> 추후 서버에 이미지 크기도 함께 저장하도록 수정
-            Popup {
-                AsyncImage(
-                    modifier = Modifier
-                        .onSizeChanged { size ->
-                            width = size.width
-                            height = size.height
-                        }
-                        .alpha(0f),
-                    model = IMAGE_BASE_URL + item.itemImages.first().fileUrl,
-                    contentDescription = null
-                )
-            }
+            var itemSize by remember { mutableStateOf(IntSize.Zero) }
 
-            if (item.categoryId !in setOf(1, 6)) {
-                if (item.itemImages.first().itemImagePositions.isNotEmpty()) {
+            SubcomposeLayout { constraints ->
+
+                val unconstrainedConstraints = constraints.copy(
+                    minWidth = 0,
+                    minHeight = 0,
+                    maxWidth = Int.MAX_VALUE,
+                    maxHeight = Int.MAX_VALUE
+                )
+
+                subcompose("measure") {
+                    AsyncImage(
+                        modifier = Modifier.onSizeChanged {
+                            itemSize = it
+                            println("Measured Image Size: ${itemSize.width} x ${itemSize.height}")
+                        }
+                            .alpha(0.1f),
+                        model = BuildKonfig.IMAGE_BASE_URL + item.itemImages.first().fileUrl,
+                        contentDescription = null
+                    )
+                }.first().measure(unconstrainedConstraints)
+
+                val contentMeasure = subcompose("content") {
                     AsyncImage(
                         modifier = Modifier
                             .size(
-                                width = (width / 4).gdp,
-                                height = (height / 4).gdp
+                                width = (itemSize.width / 4).gdp,
+                                height = (itemSize.height / 4).gdp
                             )
                             .offset(
-                                /* TODO(캐릭터 Level에 따라 입을 수 있는 아이템들 보여줌) */
-                                x = item.itemImages.first().itemImagePositions.find { it.level == 6 }?.positionX?.gdp ?: 0.gdp,
-                                y = item.itemImages.first().itemImagePositions.find { it.level == 6 }?.positionY?.gdp ?: 0.gdp
+                                x = item.itemImages.first().itemImagePositions.find { it.level == currentLevel }?.positionX?.gdp ?: 0.gdp,
+                                y = item.itemImages.first().itemImagePositions.find { it.level == currentLevel }?.positionY?.gdp ?: 0.gdp
                             ),
-                        model = IMAGE_BASE_URL + item.itemImages.first().fileUrl,
+                        model = BuildKonfig.IMAGE_BASE_URL + item.itemImages.first().fileUrl,
                         contentDescription = null
                     )
+                }.first().measure(constraints)
+
+
+                if (item.categoryId != 6) {
+                    layout(contentMeasure.width, contentMeasure.height) {
+                        contentMeasure.place(0, 0)
+                    }
+                } else {
+                    layout(0, 0) {}
                 }
             }
         }
