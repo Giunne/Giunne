08a37d5b2cfc27e1dd60b16632bb3bfa03c559e6
Joonhoo <joonhoo0123@gmail.com>
2025-03-30 17:00:49 +0900

    fix: 테스트 id 대신 실제 사용하는 recreationId로 변경

--- Giunne/common/src/commonMain/kotlin/com/project/giunne/common/presentation/roadmap/teacher/TeacherJoggingRoadmapScreen.kt
+++ Giunne/common/src/commonMain/kotlin/com/project/giunne/common/presentation/roadmap/teacher/TeacherJoggingRoadmapScreen.kt
@@ -29,6 +29,7 @@ import com.project.giunne.common.presentation.roadmap.content.TeacherCheckbox
 import com.project.giunne.common.presentation.roadmap.content.TeacherJoggingDialog
 import com.project.giunne.common.presentation.roadmap.teacher.state.RoadMapState
 import com.project.giunne.common.ui.theme.GPColor
+import com.project.giunne.common.util.AvatarUtil
 import com.project.giunne.common.util.GPFontFamily
 import com.project.giunne.common.util.gdp
 import com.project.giunne.common.util.gsp
@@ -55,8 +56,7 @@ fun TeacherJoggingRoadmapScreen(
     LaunchedEffect(isShow) {
         if (isChecked && isShow) {
             roadMapComponent.loadStudentList(
-                /* TODO(선생님용 Recreation 접속 API 나오면 ID로 변경) */
-                recreationId = 18,
+                recreationId = AvatarUtil.uiState.value.recreationId.toLong(),
                 id = courseInfo.id
             )
         }
